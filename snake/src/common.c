9a28a9d711fa681e09036b1e31635675abfca045
Sofia Vaca Narvaja <sofia_vaca_narvaja@brown.edu>
2023-02-09 10:53:37 -0500

    trial6

--- snake/src/common.c
+++ snake/src/common.c
@@ -11,11 +11,17 @@
 // extern int g_snake_column;
 // extern int g_snake_rowm;
 
-extern g_game_over = 0;  // 1 if game is over, 0 otherwise
-extern g_score = 0;      // game score: 1 point for every food eaten
-extern g_direction = INPUT_RIGHT; // direction of the snake
-extern g_snake_column = 2; //horizontal position of snake
-extern g_snake_row = 2; //vertical position of snake
+// extern g_game_over = 0;  // 1 if game is over, 0 otherwise
+// extern g_score = 0;      // game score: 1 point for every food eaten
+// extern g_direction = INPUT_RIGHT; // direction of the snake
+// extern g_snake_column = 2; //horizontal position of snake
+// extern g_snake_row = 2; //vertical position of snake
+
+g_game_over = 0;  // 1 if game is over, 0 otherwise
+g_score = 0;      // game score: 1 point for every food eaten
+g_direction = INPUT_RIGHT; // direction of the snake
+g_snake_column = 2; //horizontal position of snake
+g_snake_row = 2; //vertical position of snake
 
 /** Sets the seed for random number generation.
  * Arguments:
